33f0868af55a36c7d295af12ba08fa36b1bbb9a3
simon ryzen5 <nitrologic@gmail.com>
2025-04-12 11:58:58 +1200

    grrr

--- test.bat
+++ test.bat
@@ -3,4 +3,5 @@ set "folder=%~dp0"
 set "folder=%folder:~0,-1%"
 set "url=file:///%folder:\=/%/vsynth-demo.html"
 set "flags=--disable-web-security --allow-file-access-from-files --unsafely-treat-insecure-origin-as-secure"
-start chrome %flags% %url%
+rem start chrome %flags% %url%
+"C:\Program Files\Google\Chrome\Application\chrome.exe" %flags% %url%
